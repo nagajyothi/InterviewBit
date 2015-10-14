5afa0fd22d80243551f0947cacf20c5d1e81faed
Nagajyothi Gunti <nagajyothi.gunti@gmail.com>
2015-10-14 16:32:51 -0400

    add main method

--- Linkedin/EditDistance.java
+++ Linkedin/EditDistance.java
@@ -1,21 +1,21 @@
 /*
  * Given two words A and B, find the minimum number of steps required to convert A to B. (each operation is counted as 1 step.)
-
-You have the following 3 operations permitted on a word:
-
-Insert a character
-Delete a character
-Replace a character
-Example : 
-edit distance between
-"Anshuman" and "Antihuman" is 2.
-
-Operation 1: Replace s with t.
-Operation 2: Insert i.
+ * 
+ You have the following 3 operations permitted on a word:
+ 
+ Insert a character
+ Delete a character
+ Replace a character
+ Example : 
+ edit distance between
+ "Anshuman" and "Antihuman" is 2.
+ 
+ Operation 1: Replace s with t.
+ Operation 2: Insert i.
  */
 public class EditDistance {
-   public int minDistance(String a, String b) {
-     int aLength = a.length();
+    public static int minDistance(String a, String b) {
+        int aLength = a.length();
         int bLength = b.length();
         if(aLength == 0){
             return bLength;
@@ -42,12 +42,17 @@ public class EditDistance {
                                                table[i][j] + 1);
             }
         }
-        // for(int i = 0; i <= aLength; i++){
-        //     for(int j = 0; j <= bLength; j++){
-        //         System.out.print(table[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
+         for(int i = 0; i <= aLength; i++){
+             for(int j = 0; j <= bLength; j++){
+                 System.out.print(table[i][j] + " ");
+             }
+             System.out.println();
+         }
         return table[aLength][bLength];
- }  
+    }  
+    public static void main(String[] args) {
+        String a = "Anshuman";
+        String b = "Antihuman";
+        System.out.println(minDistance(a, b));
+    }
 }
