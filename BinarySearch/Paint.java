a7eb5b11c8b7545e71c0acf8b1a66b280696e568
Nagajyothi Gunti <nagajyothi.gunti@gmail.com>
2015-10-23 16:17:00 -0400

    Paint k boards

--- BinarySearch/Paint.java
+++ BinarySearch/Paint.java
@@ -27,7 +27,7 @@
  */
 import java.util.*;
 public class Paint{
-    public static int getMax(ArrayList<Integer> C, int n){
+    public static long getMax(ArrayList<Integer> C, int n){
         int max = Integer.MIN_VALUE;
         for(int i = 0; i < n; i++){
             if(C.get(i) > max)
@@ -36,7 +36,7 @@ public class Paint{
         return max;
     }
     
-    public static int getSum(ArrayList<Integer> C, int n){
+    public static long getSum(ArrayList<Integer> C, int n){
         int total = 0;
         for(int i = 0; i < n; i++){
             total += C.get(i);
@@ -47,8 +47,9 @@ public class Paint{
     public static int getRequiredPainters(ArrayList<Integer> C, int n, int maxLengthPerPainter){
         int total = 0;
         int numPainters = 1;
+        System.out.println("\t In get required painters maxLengthPerPainter: " + maxLengthPerPainter); 
         for(int i =0;i < n; i++){
-            total += C.get(i);
+            total += C.get(i);            
             if(total > maxLengthPerPainter){
                 total = C.get(i);
                 numPainters++;
@@ -57,28 +58,50 @@ public class Paint{
         }
         return numPainters;
     }
-    public static int partition(ArrayList<Integer> C, int n, int k){
-        int lo = getMax(C, n);
-        int hi = getSum(C, n);
+    public static long partition(ArrayList<Integer> C, int n, int k){
+        long lo = getMax(C, n);
+        System.out.println("Max = " + lo); 
+        long hi = getSum(C, n);
+        System.out.println("getSum = " + hi);
         while(lo < hi){
-            int mid = lo + (hi-lo)/2;
+            int mid = (int)(lo + (hi-lo)/2);
+            System.out.println("For Mid = " + mid +" \n lo = " + lo + "\n hi = " +  hi);
             int requiredPainters = getRequiredPainters(C, n, mid);
-            if(requiredPainters <= k)
+            System.out.println("\t required painters : " + requiredPainters);
+            if(requiredPainters <= k) {                
                 hi = mid;
-            else
+                System.out.println("\t hi = " + hi);
+            }
+            else {                
                 lo = mid + 1;
+                System.out.println("\t lo = " + lo);
+            }
         }
         return lo;
     }
     public static int paint(int A, int B, ArrayList<Integer> C) {
-        return (int)(partition(C, C.size(), A) * B);
+        return (int)(partition(C, C.size(), A) * B % 10000003);
     }
+    
+    
+    
     public static void main(String[] args){
         int K = Integer.parseInt(args[0]); // Number of painters
         int T = Integer.parseInt(args[1]); // Time taken by painter to paint 1 unit of board
         ArrayList<Integer> C = new ArrayList<Integer>();
-        C.add(1000000);
-        C.add(1000000);
+//        C.add(1000000);
+//        C.add(1000000);
+        C.add(100);
+        C.add(200);
+        C.add(300);
+        C.add(400);
+        C.add(500);
+        C.add(600);
+        C.add(700);
+        C.add(800);
+        C.add(900);
+    
+        
         System.out.println(paint(K, T, C));
     }
 }
